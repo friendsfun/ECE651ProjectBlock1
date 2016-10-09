0330e327928f740c72cde440fc2eeedbadf48db9
friendsfun <serena@seranadeMacBook-Pro.local>
2016-10-09 16:22:58 -0400

    My first commit

---
+++ LogInBlock/app/src/main/java/com/example/serena/loginblock/PageAfterLoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.serena.loginblock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PageAfterLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_after_login);
+    }
+}
